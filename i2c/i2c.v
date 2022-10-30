1c53eff4dddfbac514b35e6ec4df706ac45e37c9
Krutika Nerurkar <115050300+krutikanerurkar@users.noreply.github.com>
2022-10-29 20:55:15 -0500

    Update i2c.v

--- i2c/i2c.v
+++ i2c/i2c.v
@@ -7,7 +7,7 @@ module i2c(clk,
            avalon_read,
            avalon_readdata,
            chipselect,
-		address,
+		     address,
            read,
            readdata,
            write,
@@ -19,15 +19,15 @@ module i2c(clk,
 
 
 
-    parameter W = 8;
+    parameter W = 8;// width of the FiFo
     
-    parameter M = 6;
+    parameter M = 7; // N = 64 (depth of FiFO) check the Fifo i2C module for better understanding
 
 
     input   clk, reset;
     
 
-    // Avalon MM interface (8 word aperature)
+    // Avalon MM interface (4 word aperature)
     input             avalon_chipselect,avalon_read,avalon_write;
     input [1:0]       avalon_address;
     input [31:0]      avalon_writedata;
@@ -38,9 +38,6 @@ module i2c(clk,
     output [4:0]      readdata;	 
          
            
-    
-    // gpio interface
-    //inout reg [31:0]  data;
     output I2C_SCL;
     output I2C_SDA;
     output I2C_TEST_OUT;
@@ -51,10 +48,6 @@ module i2c(clk,
     reg [31:0]status;
     reg [31:0]control; 
 
-
-
-   // wire write_fifo;
-   // wire read_fifo;
     wire [7:0] rd_data;
     wire [31:0]rd_status;
     wire [5:0] wr_index;
@@ -76,6 +69,10 @@ module i2c(clk,
     assign RX_FE = 1'b0;
     assign ACK_ERROR = 1'b0;
     assign BUSY      = 1'b0;
+	 
+	 reg read_edge;
+	 
+	 reg write_edge;
     
     
       
@@ -83,14 +80,10 @@ module i2c(clk,
     
     // register map
     // ofs  fn
-    //   0  data (r/w)
-    //   4  out (r/w)
-    //   8  od (r/w)
-    //  12  int_enable (r/w)
-    //  16  int_positive (r/w)
-    //  20  int_negative (r/w)
-    //  24  int_edge_mode (r/w)
-    //  28  int_status_clear (r/w1c)
+    //   0  ADDRESS (r/w)
+    //   4  DATA    (r/w)
+    //   8  STATUS  (r/w1c)
+    //   16 CONTROL (r/w)
     
     // register numbers
       parameter  ADDRESS_REG    =     2'b00;
@@ -103,29 +96,37 @@ module i2c(clk,
     always @ (*)
     begin
         if (avalon_read &&  avalon_chipselect)
+		  begin
             case (avalon_address)
                ADDRESS_REG: 
                   avalon_readdata = int_address;
                DATA_REG: 
-                  avalon_readdata = rd_data;
+					   begin
+					    read_edge       = 1'b1;
+                   avalon_readdata = rd_data;
+						end
                STATUS_REG:
                   avalon_readdata = rd_status;
                CONTROL_REG:
                   avalon_readdata = control;
             endcase
-        else
+		   end
+         else
+		   begin
             avalon_readdata = 32'b0;
+				read_edge       = 1'b0;
+			end
     end        
 
-    // write register
+    // write register 
     always @ (posedge clk)
     begin
         if (reset)
         begin
-            int_address[31:0] <= 32'b0;
-            data[31:0]        <= 32'b0;
-		 status[31:0]      <= 32'b0;
-		 control[31:0]     <= 32'b0;
+            int_address[6:0]       <= 7'b0;
+            data[7:0]              <= 8'b0;
+		      status[31:0]           <= 32'b0;
+		      control[31:0]          <= 32'b0;
         end
         else
         begin
@@ -135,29 +136,33 @@ module i2c(clk,
                      ADDRESS_REG: 
                        int_address  <= avalon_writedata;
                      DATA_REG: 
-                       data         <= avalon_writedata;
+							  begin
+                        data         <= avalon_writedata;
+							   write_edge   <= 1'b1; s
+							  end
                      STATUS_REG:
                        status       <= avalon_writedata;
                      CONTROL_REG:
                        control      <= avalon_writedata;
                 endcase
             end
+				else 
+				  write_edge   <= 1'b0;
         end
     end
 
 
-//assign control_write_read = control[0];
 
 assign CLEAR = status[3];// clear the status overflow requests
 
-edgedetector wr(.i_clk(clk),.i_signal(avalon_write),.o_signal(write_fifo));
+edgedetector wr(.i_clk(clk),.i_signal(write_edge),.o_signal(write_fifo));
 
-edgedetector rd(.i_clk(clk),.i_signal(avalon_read),.o_signal(read_fifo));
-
-
+edgedetector rd(.i_clk(clk),.i_signal(read_edge),.o_signal(read_fifo));
 
 
-fifo_i2c #(.W(W),.M(M))transmit_fifo 
+
+
+fifo_i2c #(.W(W),.M(M))transmit_fifo // transmit fifo
 (.i_clk(clk),
  .reset(reset),
  .wr_data(data),
@@ -171,7 +176,7 @@ fifo_i2c #(.W(W),.M(M))transmit_fifo
  .wr_index(wr_index),
  .rd_index(rd_index));
 
-
+// status register (read) 
 assign rd_status = {12'b000000000000,rd_index,wr_index,BUSY,ACK_ERROR,EMPTY,FULL,OVERFLOW,RX_FE,RX_FF,RX_FO};
 
 
